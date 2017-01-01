047d825709a640038e66942be0d7dcce6820f1bf
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-12-31 19:02:07 -0800

    minor stuff, prepping to deal with dupes

--- src/base.js
+++ src/base.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('matrix-puppet:base');
+const debug = (...args) => require('debug')(['matrix-puppet','base',...args].join(':'));
 const Promise = require('bluebird');
 const { Bridge, MatrixRoom, RemoteRoom } = require('matrix-appservice-bridge');
 
@@ -13,7 +13,7 @@ class Base {
           console.log('got user query', queriedUser);
           return {}; // auto provision users w no additional data
         },
-        onEvent: this.handleMatrixEvent,
+        onEvent: this.handleMatrixEvent.bind(this),
         onAliasQuery: function() {
           console.log('on alias query');
         },
@@ -74,25 +74,26 @@ class Base {
     return this.bridge.getIntent();
   }
   getOrCreateMatrixRoomFromThirdPartyRoomId(thirdPartyRoomId) {
+    const info = debug('getOrCreateMatrixRoomFromThirdPartyRoomId');
     const autoJoinNewRoom = true; // false was not tested
     const roomStore = this.bridge.getRoomStore();
     const roomAlias = this.getRoomAliasFromThirdPartyRoomId(thirdPartyRoomId);
     return roomStore.getEntryById(roomAlias).then(entry=>{
-      debug("get or otherwise create the matrix room");
+      info("get or otherwise create the matrix room");
       if ( entry ) return entry;
       else {
-        debug("it is not in our entry, so lets get the third party info for now so we have it");
+        info("it is not in our entry, so lets get the third party info for now so we have it");
         return this.getThirdPartyRoomDataById(thirdPartyRoomId).then(thirdPartyRoomData => {
-          debug("our local cache may be empty, so we should find out if this room");
-          debug("is already on matrix and get that first using the room alias");
+          info("our local cache may be empty, so we should find out if this room");
+          info("is already on matrix and get that first using the room alias");
           const botIntent = this.getIntentFromApplicationServerBot();
           const botClient = botIntent.getClient();
           return botClient.getRoomIdForAlias(roomAlias).then(({room_id}) => {
-            debug("we got the room ID. so it exists on matrix.");
-            debug("we just need to update our local cache, return the matrix room id for now");
+            info("we got the room ID. so it exists on matrix.");
+            info("we just need to update our local cache, return the matrix room id for now");
             return room_id;
           }, (_err) => {
-            debug("the room doesn't exist. we need to create it for the first time");
+            info("the room doesn't exist. we need to create it for the first time");
             return botIntent.createRoom({ createAsClient: true }).then(({room_id}) => {
               return Promise.all([
                 botIntent.createAlias(roomAlias, room_id),
@@ -101,19 +102,19 @@ class Base {
                 autoJoinNewRoom ? this.puppet.getClient().joinRoom(room_id) : botIntent.invite(room_id, this.puppet.id),
                 botIntent.setPowerLevel(room_id, this.puppet.id, 100)
               ]).then(()=>{
-                debug("now return the matrix room id so we can use it to update the cache");
+                info("now return the matrix room id so we can use it to update the cache");
                 return room_id;
               });
             });
           });
         }).then(matrixRoomId => {
-          debug("now's the time to update our local cache for this linked room");
+          info("now's the time to update our local cache for this linked room");
           return roomStore.upsertEntry({
             id: roomAlias,
             remote: new RemoteRoom(thirdPartyRoomId),
             matrix: new MatrixRoom(matrixRoomId)
           }).then(()=> {
-            debug("finally return the entry we were looking for in the first place");
+            info("finally return the entry we were looking for in the first place");
             return roomStore.getEntryById(roomAlias);
           });
         });
@@ -121,7 +122,8 @@ class Base {
     });
   }
   handleThirdPartyRoomMessage(thirdPartyRoomMessageData) {
-    debug('handling third party room message data', thirdPartyRoomMessageData);
+    const info = debug('handleThirdPartyRoomMessage');
+    info('handling third party room message data', thirdPartyRoomMessageData);
     const {
       thirdParty: {
         roomId,
@@ -133,14 +135,14 @@ class Base {
       text
     } = thirdPartyRoomMessageData;
     return this.getOrCreateMatrixRoomFromThirdPartyRoomId(roomId).then((entry)=> {
-      debug("room store entry", entry);
+      info("room store entry", entry);
       if ( senderId === this.getPuppetThirdPartyUserId() ) {
-        debug("this message was sent by me, send it to the matrix room via puppet as notice");
+        info("this message was sent by me, send it to the matrix room via puppet as notice");
         return this.puppet.getClient().joinRoom(entry.matrix.roomId).then(()=>{
           this.puppet.getClient().sendNotice(entry.matrix.roomId, text);
         });
       } else {
-        debug("this message was not sent by me, send it the matrix room via ghost user as text");
+        info("this message was not sent by me, send it the matrix room via ghost user as text");
         const ghostIntent = this.getIntentFromThirdPartySenderId(senderId);
         return Promise.mapSeries([
           () => ghostIntent.setDisplayName(senderName),
@@ -150,8 +152,18 @@ class Base {
       }
     });
   }
-  handleMatrixEvent(req, _context) { // uses groupme client
-    console.log('handle matrix event type', req.getData().type);
+  handleMatrixEvent(req, _context) {
+    const info = debug(this.handleMatrixEvent.name, 'info');
+    const error = debug(this.handleMatrixEvent.name, 'error');
+    const data = req.getData();
+    info('got incoming matrix event of type', data.type);
+    switch (data.type) {
+      case 'm.room.message': return this.handleMatrixMessageEvent();
+      default: return error('ignored last matrix event');
+    }
+  }
+  handleMatrixMessageEvent() {
+
   }
 }
 
