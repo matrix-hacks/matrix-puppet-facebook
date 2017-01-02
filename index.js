96a65890bcde902d47fb7800bff31cae9083f2f8
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-01-02 09:48:42 -0800

    it works again

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const path = require('path');
-
-const config = require('./config.json');
 const {
   MatrixAppServiceBridge: {
     Cli, AppServiceRegistration
@@ -8,72 +5,42 @@ const {
   Puppet,
   MatrixPuppetBridgeBase
 } = require("matrix-puppet-bridge");
-
+const GroupMeClient = require('./client');
+const config = require('./config.json');
+const path = require('path');
 const puppet = new Puppet(path.join(__dirname, './config.json' ));
 
-const GroupMeClient = require('./client');
-
 class App extends MatrixPuppetBridgeBase {
+  getServicePrefix() {
+    return "groupme";
+  }
   initThirdPartyClient() {
     this.thirdPartyClient = new GroupMeClient(this.config.groupme.accessToken);
     return this.thirdPartyClient.connect().then(() => {
       return this.thirdPartyClient.api.getMe();
     }).then(user => {
-      this.thirdPartyUserId = user.id;
-      return this.thirdPartyClient.subscribe(`/user/${user.id}`);
-    }).then(userSub => {
-      console.log('Subscribed to GroupMe user messages');
-      userSub.on('line.create', (data) => {
-        const thirdParty = this.mapThirdPartyRoomMessageData(data);
-        return this.handleThirdPartyRoomMessage(thirdParty);
+      return this.thirdPartyClient.subscribe(`/user/${user.id}`).then(userSub => {
+        console.log('Subscribed to GroupMe user messages');
+        userSub.on('line.create', (data) => {
+          const { subject: { id, group_id, user_id, text, name } } = data;
+          return this.handleThirdPartyRoomMessage({
+            roomId: group_id,
+            messageId: id,
+            senderName: name,
+            senderId: user_id === user.id ? undefined : user_id,
+            text
+          });
+        });
       });
     });
   }
-  // GroupMe can handle a discreet deduplication tag
-  defaultDeduplicationTag() {
-    return "\u2063";
-  }
-  defaultDeduplicationTagPattern() {
-    return "\\u2063$";
-  }
-  getPuppetThirdPartyUserId() {
-    return this.thirdPartyUserId;
-  }
-  getServicePrefix() {
-    return "groupme";
-  }
   getThirdPartyRoomDataById(id) {
-    return this.thirdPartyClient.api.showGroup(id).then(data=>{
-      return {
-        name: data.name,
-        topic: data.description,
-      };
-    });
-  }
-  /**
-   * Converts the third party service's room message data object to that which we expect in our App
-   *
-   * @param {object} thirdPartyData Third party's representation of a room message
-   * @returns {object} App's representation of a third party room message
-   */
-  mapThirdPartyRoomMessageData(thirdPartyData) {
-    const {
-      subject: { id, group_id, user_id, text, name, picture_url }
-    } = thirdPartyData;
-    return {
-      thirdParty: {
-        roomId: group_id,
-        messageId: id,
-        senderName: name,
-        senderId: user_id,
-      },
-      attachmentUrl: picture_url,
-      text
-    };
+    return this.thirdPartyClient.api.showGroup(id).then(data=>({
+      name: data.name, topic: data.description
+    }));
   }
   sendMessageAsPuppetToThirdPartyRoomWithId(id, text) {
-    const sendMessage = this.thirdPartyClient.api.sendGroupMessage(id);
-    return sendMessage(text);
+    return this.thirdPartyClient.api.sendGroupMessage(id)(text);
   }
 }
 
@@ -86,7 +53,7 @@ new Cli({
       reg.setHomeserverToken(AppServiceRegistration.generateToken());
       reg.setAppServiceToken(AppServiceRegistration.generateToken());
       reg.setSenderLocalpart("groupmebot");
-      reg.addRegexPattern("users", "@__mpb__groupme_.*", true);
+      reg.addRegexPattern("users", "@groupme_.*", true);
       callback(reg);
     }).catch(err=>{
       console.error(err.message);
